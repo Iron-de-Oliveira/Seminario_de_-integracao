f456aa14b7d54a7eae9e10961cc23bd4593fcd5a
name <97321565+Jbsousa86@users.noreply.github.com>
2025-05-09 14:39:57 -0300

    add html e css de cadastro

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-from flask import flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify
+
+app = Flask (__name__)
+
+@app.route("/")
+def home():
+    return "<h1>Bem-vindo ao Projeto Integrador!</h1><p>Acesse <a href='/cadastro'>/cadastro</a></p>"
+
+
+@app.route ("/cadastro")
+def cadastro():
+    return render_template('cadastro.html')
+
+if __name__ == '__main__':
+   app.run(debug = True)
