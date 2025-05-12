4b0af041d2d6d15ff423150db3697c0d476bc8b2
Iron de Oliviera Cardoso <irondeoliveira3@gmail.com>
2025-05-12 12:44:12 +0000

    criação back

--- app.py
+++ app.py
@@ -2,14 +2,27 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask (__name__)
 
+# rotas templates
 @app.route("/")
-def home():
+def home1():
     return "<h1>Bem-vindo ao Projeto Integrador!</h1><p>Acesse <a href='/cadastro'>/cadastro</a></p>"
 
+@app.route("/home.html")
+def home():
+    return render_template("home.html")
 
 @app.route ("/cadastro")
 def cadastro():
     return render_template('cadastro.html')
+
+
+# rotas APIs
+# NOVO USUÁRIO
+@app.route("/usuario", methods=['POST'])
+def novo_usuario():
+ data = request.get_json()
+ print("Recebido do front-end:", data)
+
 
 if __name__ == '__main__':
    app.run(debug = True)
---
+++ static/js/cadastro.js
@@ -0,0 +1,36 @@
+function coletar_dados(){
+    const data_nome = document.getElementById("name").value;
+    const data_email = document.getElementById("email").value;
+    const data_senha = document.getElementById("password").value;
+    const data_local = document.getElementByID("localizacao").value;
+
+    const dados_usuario = {
+        nome: data_nome,
+        email: data_email,
+        senha: data_senha,
+        local: data_local
+    };
+
+     fetch('http://localhost:5000/usuario', {
+
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(dados_usuario)
+    })
+    .then(response => response.json())
+    .then(data => {
+        alert(data.message);
+    })
+    .catch(error => {
+        console.error('Erro:', error);
+        alert("Erro ao cadastrar novo usuário");
+    });
+
+    var lista = document.getElementById("form");
+  
+    lista.selectedIndex = 0;
+
+    window.location.href = "login_cadastro.html"
+}    
